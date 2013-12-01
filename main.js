778a428883ccd682b2cf264f2d74b4f7992377a5
Winston Fassett <winston.fassett@gmail.com>
2013-12-01 13:58:13 -0600

    nesting example

--- main.js
+++ main.js
@@ -20,7 +20,100 @@
         'Aiden'
     ];
 
+    var Node = function(data, parent){
+        var self = this;
+        this.parent = ko.observable(parent);
+        this.name = ko.observable(data.name);
+        this.children = ko.observableArray((data.children||[]).map(function(it){
+            return new Node(it, self);
+        }));
+        this.parents = ko.computed({
+            // deferEvaluation: true,
+            read: function(){
+                var parents = []
+                  , parent = self.parent();
+                while(parent){
+                    parents.push(parent);
+                    parent = parent.parent();
+                }
+                return parents;
+            }
+        })
+        this.dragging = ko.observable();
+    }
+
     var model = {
+        hierarchy: {
+            node: new Node({
+                name: 'Root',
+                children: [
+                    { name: 'A', children: [
+                        { name: 'Child 1' },
+                        { name: 'Child 2' },
+                        { name: 'Child 3' },
+                    ]},
+                    { name: 'B', children: [
+                        { name: 'Child 1' },
+                        { name: 'Child 2' },
+                        { name: 'Child 3' },
+                    ]},
+                    { name: 'C', children: [
+                        { name: 'Child 1' },
+                        { name: 'Child 2' },
+                        { name: 'Child 3' },
+                    ]},
+                    { name: 'D', children: [
+                        { name: 'Child 1' },
+                        { name: 'Child 2' },
+                        { name: 'Child 3' },
+                    ]}
+                ]
+            }),
+            dragStart: function (item) {
+                item.dragging(true);
+            },
+            dragEnd: function (item) {
+                item.dragging(false);
+            },
+            dragEnter: function (event, data, model) {
+                if(data==model)
+                    return false;
+                var parents = model.parents();
+                if(parents.indexOf(data) >=0)
+                    return false;
+                // var match = data.value.match(/^(a|e|i|o|u|y)/i);
+
+                // return !!match;
+            },            
+            reorder: function (event, dragData, zoneData) {
+                if (dragData !== zoneData) {
+                    var zoneDataIndex = model.sortable.items.indexOf(zoneData);
+                    model.sortable.items.remove(dragData);
+                    model.sortable.items.splice(zoneDataIndex, 0, dragData);
+                }
+            },            
+            source: ko.observableArray([].concat(names)),
+            target: ko.observableArray(),
+            dropFromSource: function (data, model) {
+                var parent = data.parent && data.parent();
+                if(parent){
+                    parent.children.remove(data)
+                    data.parent(null)
+                }
+                model.source.remove(data);
+                model.target.push(data);
+            },
+            dropFromTarget: function (node, target) {
+                var parent = ko.utils.unwrapObservable(node.parent);
+                if(parent){
+                    parent.children.remove(node);
+                }
+                target.children.push(node);
+                node.parent(target);
+                // model.target.remove(data);
+                // model.source.push(data);
+            }
+        },
         simple: {
             source: ko.observableArray([].concat(names)),
             target: ko.observableArray(),
